0f86ff7288a47f61bf60dc939cf2b8a20a549cac
winston salem <winstonwill94@gmail.com>
2022-05-11 20:21:49 -0300

    make it better

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from ".prisma/client";
 // configuraçao
 const prismaClient =  new PrismaClient();
 
+require("dotenv").config();
+
 export default prismaClient;
 
 // com este voce consegue fazer um crud total
--- src/services/users/authUserService.ts
+++ src/services/users/authUserService.ts
@@ -26,7 +26,7 @@ class AuthUserServie {
             name: user.name,
             email: user.email
         },
-            ""+process.env.JWT_KEY,
+            process.env.JWT_KEY,
             {
                 subject: user.id,
                 expiresIn: '30d'
