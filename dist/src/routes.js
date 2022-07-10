b41de295e88fb47064470633c1ef60ff1d942839
winston salem <winstonwill94@gmail.com>
2022-07-10 17:20:09 -0300

    projeto

--- dist/src/routes.js
+++ dist/src/routes.js
@@ -1,13 +1,26 @@
+// "use strict";
+// Object.defineProperty(exports, "__esModule", { value: true });
+// exports.router = void 0;
+// const express_1 = require("express"); // trabalhar com roteameto
+// const UserController_1 = require("./controllers/users/UserController");
+// const AuthUserController_1 = require("./controllers/users/AuthUserController");
+// const router = (0, express_1.Router)();
+// exports.router = router;
+// router.post('/users', new UserController_1.CreatUserController().handle);
+// router.post('/session', new AuthUserController_1.AuthUserController().handle);
+
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//     return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express"); // trabalhar com roteameto
 const multer_1 = __importDefault(require("multer"));
 const UserController_1 = require("./controllers/users/UserController");
 const AuthUserController_1 = require("./controllers/users/AuthUserController");
+
+
 const DetailUserController_1 = require("./controllers/users/DetailUserController");
 const ValidAuthenticated_1 = require("./middleware/ValidAuthenticated");
 const CategoryController_1 = require("./controllers/category/CategoryController");
