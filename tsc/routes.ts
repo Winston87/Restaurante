40e9eba3924012898645dc9c77dc7b844777a150
winston salem <winstonwill94@gmail.com>
2022-07-28 20:33:26 -0300

    putItem

---
+++ tsc/routes.ts
@@ -0,0 +1,4 @@
+import { PutOrderItemController } from "./controllers/orders/PutOrderItemController";
+
+
+router.put('/order/item',ValidAuth, new PutOrderItemController().handle);// atualizar quantidade do item no pedido
